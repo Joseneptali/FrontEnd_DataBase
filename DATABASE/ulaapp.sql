c304633a6af62b8e47eba969d0c27001d2fb08ca
Joseneptali <ingjoserosania@gmail.com>
2024-05-24 21:29:34 -0500

    update sql database

--- DATABASE/ulaapp.sql
+++ DATABASE/ulaapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-03-2024 a las 20:27:04
+-- Tiempo de generación: 25-05-2024 a las 03:48:01
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -39,6 +39,14 @@ CREATE TABLE `citas` (
   `mensaje` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`codigo_cita`, `nombre_paciente`, `especialidad`, `medico`, `fecha`, `tiempo`, `email`, `numero_telefono`, `mensaje`) VALUES
+(0, 'liliana ordoez', 'oftalmologia', 'orlando', '2024-05-31', '03:33:18', 'liliana@gmail.com', '3017220545', NULL),
+(1, 'jose rosania', 'oftalmologia', 'rafael donado', '2024-04-24', '19:23:32', 'ingjoserosania@gmail.com', '3003120224', 'paciente presenta problema de retina');
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +60,13 @@ CREATE TABLE `especialidades` (
   `estado` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `especialidades`
+--
+
+INSERT INTO `especialidades` (`id`, `especialidad`, `descripcion`, `estado`) VALUES
+(1, 'oftalmologia', 'especialidad de los ojos', 'activo');
+
 -- --------------------------------------------------------
 
 --
@@ -62,7 +77,6 @@ CREATE TABLE `medico` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
   `apellido` varchar(255) NOT NULL,
-  `usuario` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
   `fecha_nacimiento` date DEFAULT NULL,
@@ -74,6 +88,13 @@ CREATE TABLE `medico` (
   `telefono` varchar(20) DEFAULT NULL,
   `estado` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `medico`
+--
+
+INSERT INTO `medico` (`id`, `nombre`, `apellido`, `email`, `contraseña`, `fecha_nacimiento`, `sexo`, `direccion`, `pais`, `ciudad`, `codigo_postal`, `telefono`, `estado`) VALUES
+(1, 'JOSE', 'ROSANIA', 'ingjoserosania@gmail.com', '1234565672*', NULL, NULL, 'calle 29', 'colombia', 'barranquilla', '080001', '3003120224', 'casado');
 
 -- --------------------------------------------------------
 
@@ -85,7 +106,6 @@ CREATE TABLE `pacientes` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
   `apellido` varchar(255) NOT NULL,
-  `usuario` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
   `fecha_nacimiento` date DEFAULT NULL,
@@ -93,10 +113,33 @@ CREATE TABLE `pacientes` (
   `direccion` varchar(255) DEFAULT NULL,
   `pais` varchar(100) DEFAULT NULL,
   `ciudad` varchar(100) DEFAULT NULL,
-  `codigo_postal` varchar(10) DEFAULT NULL,
   `telefono` varchar(20) DEFAULT NULL,
   `estado` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `estado` enum('activo','inactivo') DEFAULT 'activo',
+  `rol` enum('admin','usuario','moderador') DEFAULT 'usuario',
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre_usuario`, `correo`, `contraseña`, `estado`, `rol`, `fecha_creacion`) VALUES
+(1, 'jose.rosania', 'ingjoserosania@gmail.com', '1234565672*', 'activo', 'admin', '2024-05-18 04:19:43'),
+(2, 'pedro', 'pedro@pedro.com', '1234565672*', 'activo', 'admin', '2024-05-20 05:12:29');
 
 --
 -- Índices para tablas volcadas
@@ -120,7 +163,6 @@ ALTER TABLE `especialidades`
 --
 ALTER TABLE `medico`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `usuario` (`usuario`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -128,8 +170,14 @@ ALTER TABLE `medico`
 --
 ALTER TABLE `pacientes`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `usuario` (`usuario`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -139,25 +187,31 @@ ALTER TABLE `pacientes`
 -- AUTO_INCREMENT de la tabla `citas`
 --
 ALTER TABLE `citas`
-  MODIFY `codigo_cita` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `codigo_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `especialidades`
 --
 ALTER TABLE `especialidades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `medico`
 --
 ALTER TABLE `medico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8800327;
 
 --
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
